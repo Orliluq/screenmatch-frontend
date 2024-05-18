ac50b286508b9717f97196bed2326687615f6b48
Orli Dun <122529721+Orliluq@users.noreply.github.com>
2024-05-17 21:20:20 -0400

    Update Categoria.java

--- src/main/java/com/aluracursos/screenmatch/model/Categoria.java
+++ src/main/java/com/aluracursos/screenmatch/model/Categoria.java
@@ -6,30 +6,27 @@ public enum Categoria {
     COMEDIA("Comedy", "Comedia"),
     DRAMA("Drama", "Drama"),
     CRIMEN("Crime", "Crimen");
-
     private String categoriaOmdb;
     private String categoriaEspanol;
-    Categoria (String categoriaOmdb, String categoriaEspanol){
+    Categoria(String categoriaOmdb, String categoriaEspanol){
         this.categoriaOmdb = categoriaOmdb;
         this.categoriaEspanol = categoriaEspanol;
-    }
 
+    }
     public static Categoria fromString(String text) {
         for (Categoria categoria : Categoria.values()) {
             if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
                 return categoria;
             }
         }
-        throw new IllegalArgumentException("Ninguna categorÍa encontrada: " + text);
+        throw new IllegalArgumentException("Ninguna categoría encontrada: " + text);
     }
-
     public static Categoria fromEspanol(String text) {
         for (Categoria categoria : Categoria.values()) {
             if (categoria.categoriaEspanol.equalsIgnoreCase(text)) {
                 return categoria;
             }
         }
-        throw new IllegalArgumentException("Ninguna categorÍa encontrada: " + text);
+        throw new IllegalArgumentException("Ninguna categorìa encontrada: " + text);
     }
-
 }
